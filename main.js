5725a0ff61e152e5736e21e0a3588b1cd6bd5a7e
shayonsaleh <shayonsaleh@yahoo.com>
2022-01-03 17:08:19 -0500

    Added game over state

--- main.js
+++ main.js
@@ -10,32 +10,58 @@ let random = -1
 
 score = 0
 
+urls = [
+    'https://go.fuqua.duke.edu/angularws/rest/fuqua/directory/department/dn/gid%3D1621973478269%2Cou%3DFuquaGroups%2Co%3DFuqua%2Cc%3DUS',
+    'https://go.fuqua.duke.edu/angularws/rest/fuqua/directory/department/dn/gid%3D1621973617765%2Cou%3DFuquaGroups%2Co%3DFuqua%2Cc%3DUS',
+    'https://go.fuqua.duke.edu/angularws/rest/fuqua/directory/department/dn/gid%3D1621973936894%2Cou%3DFuquaGroups%2Co%3DFuqua%2Cc%3DUS',
+    'https://go.fuqua.duke.edu/angularws/rest/fuqua/directory/department/dn/gid%3D1621974251030%2Cou%3DFuquaGroups%2Co%3DFuqua%2Cc%3DUS',
+    'https://go.fuqua.duke.edu/angularws/rest/fuqua/directory/department/dn/gid%3D1621974382945%2Cou%3DFuquaGroups%2Co%3DFuqua%2Cc%3DUS',
+    'https://go.fuqua.duke.edu/angularws/rest/fuqua/directory/department/dn/gid%3D1621974562831%2Cou%3DFuquaGroups%2Co%3DFuqua%2Cc%3DUS'
+]
+
 fetch('https://go.fuqua.duke.edu/angularws/rest/fuqua/directory/department/dn/gid%3D1621972604806%2Cou%3DFuquaGroups%2Co%3DFuqua%2Cc%3DUS')
 .then(res => res.json())
 .then(data => {
     dataStore = data
-    members = data.theMembers
-    nextPerson(data.theMembers)
+    // members = data.theMembers
+    members = data.theMembers.slice(1,4)
+    nextPerson(members)
 })
 
 function nextPerson(members) {
-    // dataStore = data
-    // members = dataStore.theMembers
-    random = Math.floor(Math.random() * members.length)
-    // console.log(members)    
-    currMember = members[random]
-    // dataDiv.innerHTML += `<p> ${random} </p>`
-    // dataDiv.innerHTML += `<p> ${currMember.cn} </p>`
-    dataDiv.innerHTML += `<p> ${currMember.givenname} </p>`
-    // dataDiv.innerHTML += `<p> ${currMember.gradyear} </p>`
-    if(currMember.largejpegphoto){
-        // dataDiv.innerHTML += `<p> ${"http://" + currMember.largejpegphoto.substring(2)} </p>`
-        photoImg.src = "http://" + currMember.largejpegphoto.substring(2)
+    if (members.length == 0){
+        // document.getElementById('fname').readOnly = true
+        namefield = document.getElementById('fname')
+        namefield.parentNode.parentNode.removeChild(namefield.parentNode)
+        photoImg.parentNode.removeChild(photoImg)
+        consoleText.innerText = 'Console: Game Over'
+    } else {
+        // dataStore = data
+        // members = dataStore.theMembers
+        random = Math.floor(Math.random() * members.length)
+        // console.log(members)    
+        currMember = members[random]
+
+        // skip people with no photo
+        while (!currMember.largejpegphoto){
+            console.log("skipping person with no photo:" + currMember.givenname)
+            members.splice(random, 1)
+            random = Math.floor(Math.random() * members.length)   
+            currMember = members[random]
+        }
+        // dataDiv.innerHTML += `<p> ${random} </p>`
+        // dataDiv.innerHTML += `<p> ${currMember.cn} </p>`
+        dataDiv.innerHTML += `<p> ${currMember.givenname} </p>`
+        // dataDiv.innerHTML += `<p> ${currMember.gradyear} </p>`
+        if(currMember.largejpegphoto){
+            // dataDiv.innerHTML += `<p> ${"http://" + currMember.largejpegphoto.substring(2)} </p>`
+            photoImg.src = "http://" + currMember.largejpegphoto.substring(2)
+        }
+        // dataDiv.innerHTML += `<p> ${currMember.middlename} </p>`
+        dataDiv.innerHTML += `<p> ${currMember.nickname} </p>`
+        // dataDiv.innerHTML += `<p> ${currMember.sn} </p>`
+        console.log(members[random])
     }
-    // dataDiv.innerHTML += `<p> ${currMember.middlename} </p>`
-    dataDiv.innerHTML += `<p> ${currMember.nickname} </p>`
-    // dataDiv.innerHTML += `<p> ${currMember.sn} </p>`
-    console.log(members[random])
 }
 
 document.getElementById('fname').addEventListener("keyup", function(evt){
@@ -61,7 +87,7 @@ document.getElementById('fname').addEventListener("keyup", function(evt){
         // dataDiv.innerHTML += `<p> ${currMember.nickname} </p>`
         // dataDiv.innerHTML += `<p> ${currMember.sn} </p>`
     }
-    console.log(this.value)
+    //console.log(this.value)
 })
 
 document.getElementById('fname').addEventListener("keypress", function(evt){
@@ -81,7 +107,7 @@ document.getElementById('fname').addEventListener("keypress", function(evt){
         members.splice(random, 1)
         // // console.log(members.length)
 
-        // //pick next person
+        //pick next person
         nextPerson(members)
     }
 })
